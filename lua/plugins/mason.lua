31ccabd3d0e8772d0ce14c43a9d287308ee52495
M4DA <jagielski.adamm@gmail.com>
2024-06-21 09:56:14 +0200

    Python formatter change

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -21,8 +21,9 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
         "stylua",
-        "flake8",
-        "autopep8",
+        -- "flake8",
+        -- "autopep8",
+        "black",
       })
     end,
   },
